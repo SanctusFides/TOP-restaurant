d6de5a35e82e3879e9d19309e0062eb8fddd74eb
John <jhn.hines@gmail.com>
2023-06-27 14:36:00 -0500

    Reset image locations. Github has the issue w/ img

--- src/contact.js
+++ src/contact.js
@@ -18,7 +18,7 @@ export default function contact() {
 
     // Creates the image from Sesame Street
     const contactImage = document.createElement('img');
-    contactImage.src = "https://github.com/SanctusFides/TOP-restaurant/blob/main/dist/street.png?raw=true";
+    contactImage.src = "../src/street.png";
     contactImage.className = 'street-image';
     contact.appendChild(contactImage);
 
